8b8bd71bcb38db4149ee47cb335f58de3c6fcbe4
hwakabh <hrykwkbys1024@gmail.com>
2024-01-19 12:02:40 +0900

    chore: added comments for maintenance.

--- app/config.py
+++ app/config.py
@@ -2,6 +2,8 @@ from pydantic_settings import BaseSettings
 
 
 class AppSettings(BaseSettings):
+    # BaseSettings of pydantic-settings will read environmental variables first,
+    # and if they are not provided, will set default values below.
     MYSQL_HOST: str = 'localhost'
     MYSQL_USER: str = 'root'
     MYSQL_PASSWORD: str = 'root'
