1e10540be102a0c2b42867ab473e6ecb76f9a80c
hwakabh <hrykwkbys1024@gmail.com>
2024-01-22 11:10:58 +0900

    feat: enabled to fetch URL path dynamically for v0 API.

--- app/api/v0/routers.py
+++ app/api/v0/routers.py
@@ -1,13 +1,15 @@
 from fastapi import APIRouter
 from fastapi.responses import JSONResponse
+from fastapi.requests import Request
 
 
 router = APIRouter()
 
 
 @router.get('/')
-def index() -> JSONResponse:
+def index(req: Request) -> JSONResponse:
+
     return JSONResponse(content={
-        "path": "/api/v0/",
+        "path": req.url.path,
         "detail": "v0 API has been deprecated, please access v1 API with /api/v1/"
     })
