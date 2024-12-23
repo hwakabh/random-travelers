147ac4a9dc5452f9b29a24eb906fd17a1468e090
hwakabh <hrykwkbys1024@gmail.com>
2024-12-23 16:28:57 +0900

    chore(deps): replaced orm_mode with model_config.

--- app/api/v1/schemas.py
+++ app/api/v1/schemas.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, ConfigDict
 
 
 class RootResponseBase(BaseModel):
@@ -61,5 +61,4 @@ class Airport(AirportBase):
     types: str
     datasource: str
 
-    class Config:
-        orm_mode = True
+    model_config = ConfigDict()
