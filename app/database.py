33aec3da0a6be164bd63be4ee617553e306641cf
hwakabh <hrykwkbys1024@gmail.com>
2024-12-24 23:41:47 +0900

    feat: enabled to create database for first migration.

--- app/database.py
+++ app/database.py
@@ -8,29 +8,31 @@ from app.config import app_settings
 
 
 # dialect for using with mysql-connector-python
-SQLALCHEMY_DATABASE_URL = "mysql+mysqlconnector://{user}:{password}@{host}:{port}/{dbname}".format(
+ENGINE_URL = "mysql+mysqlconnector://{user}:{password}@{host}:{port}".format(
     user=app_settings.MYSQL_USER,
     password=app_settings.MYSQL_PASSWORD,
     host=app_settings.MYSQL_HOST,
     port=3306,
-    dbname=app_settings.MYSQL_DATABASE
 )
 
+# Create database for first migrations
+with create_engine(ENGINE_URL).connect() as conn:
+    conn.execute(text(f'CREATE DATABASE IF NOT EXISTS {app_settings.MYSQL_DATABASE}'))
 
 # Configurations for returing database instances
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL,
-    echo=False
+    ENGINE_URL + '/' + app_settings.MYSQL_DATABASE,
+    echo=False,
 )
 
-SessionLocal = sessionmaker(
-    autocommit=False,
-    autoflush=False,
-    bind=engine
-)
 
 def get_db():
-    db = SessionLocal()
+    db = sessionmaker(
+        autocommit=False,
+        autoflush=False,
+        bind=engine
+    )
+
     try:
         yield db
     finally:
