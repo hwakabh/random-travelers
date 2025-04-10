e8d03df0197b9ed55c021514a6909d8300d7e991
hwakabh <hrykwkbys1024@gmail.com>
2025-04-10 23:53:58 +0900

    deploy: fixed TypeError on uvicorn.run() with Heroku.

--- run.py
+++ run.py
@@ -6,10 +6,11 @@ import app
 
 
 if __name__ == '__main__':
-    # https://github.com/encode/uvicorn/blob/4fdfec4adf1ba6da5e65c8422321e203b6050052/uvicorn/main.py#L464
+    # https://github.com/encode/uvicorn/blob/master/uvicorn/main.py#L469
+    PORT: str = os.environ.get('PORT', '8000')
     uvicorn.run(
         app="app.main:app",
         host='0.0.0.0',
-        port=os.environ.get('PORT', 8000),
+        port=int(PORT),
         reload=True
     )
