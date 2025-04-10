39d3a13e0e37704ab2d917c20827b32f6020b79a
hwakabh <hrykwkbys1024@gmail.com>
2025-04-10 17:44:10 +0900

    deploy: added parse logics with JAWSDB_URL.

--- app/config.py
+++ app/config.py
@@ -1,14 +1,16 @@
 from pydantic_settings import BaseSettings
+import os
+import re
 
 
 class AppSettings(BaseSettings):
     # BaseSettings of pydantic-settings will read environmental variables first,
     # and if they are not provided, will set default values below.
-    MYSQL_HOST: str = 'localhost'
-    MYSQL_USER: str = 'root'
-    MYSQL_PASSWORD: str = 'root'
-    MYSQL_DATABASE: str = 'rt'
-    MYSQL_PORT: str = '3306'
+    MYSQL_HOST: str = re.split('[:@/]', os.environ.get('JAWSDB_URL'))[5]
+    MYSQL_USER: str = re.split('[:@/]', os.environ.get('JAWSDB_URL'))[3]
+    MYSQL_PASSWORD: str = re.split('[:@/]', os.environ.get('JAWSDB_URL'))[4]
+    MYSQL_DATABASE: str = re.split('[:@/]', os.environ.get('JAWSDB_URL'))[7]
+    MYSQL_PORT: str = re.split('[:@/]', os.environ.get('JAWSDB_URL'))[6]
 
     GOOGLE_MAPS_API_KEY: str = ''
 
