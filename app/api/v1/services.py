d895d868bbc6637531224d7b992307921542bc05
hwakabh <hrykwkbys1024@gmail.com>
2025-05-26 03:49:58 +0900

    feat: added error-handling of translate API.

--- app/api/v1/services.py
+++ app/api/v1/services.py
@@ -27,15 +27,17 @@ def load_google_map() -> Response:
 def translate_county_name(txt: str) -> str:
 
     API_KEY = app_settings.GOOGLE_MAPS_API_KEY
-    if API_KEY is None:
-        # TODO: Implement with raise error for client-side
-        print("Failed to load API KEY")
-        pass
+    if API_KEY == '':
+        print("Failed to load API KEY, will use raw response from API")
+        return ""
 
     url = f'https://translation.googleapis.com/language/translate/v2?key={API_KEY}&q={txt}&source=en&target=ja'
 
     # Spoofing referer for Cloud Translate API
     resp = httpx.post(url, headers={"Referer": "http://localhost:3000/"}).json()
+    if resp.get('error'):
+        print('Failed to fetch result from Translate API, returning bare response of API for UI.')
+        return ""
 
     return resp.get('data').get('translations')[0].get('translatedText')
 
